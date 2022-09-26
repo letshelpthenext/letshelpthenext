dd30f5ee68728a37186a6343bace6294cbcab8fa
Kwaku Appau-Nkansah <brakwakujr@gmail.com>
2022-09-25 23:39:00 -0600

    Style hero text

--- src/components/Hero/hero.styles.js
+++ src/components/Hero/hero.styles.js
@@ -4,11 +4,31 @@ export const MainWrapper = styled.div`
   min-height: 100vh;
   background: linear-gradient(
       to bottom,
-      rgba(0, 0, 0, 0.73),
-      rgba(0, 0, 0, 0.7)
+      rgba(0, 0, 0, 0.2),
+      rgba(0, 0, 0, 0.4)
     ),
-    url(${(props)=>props.wallImage}) center no-repeat;
+    url(${(props) => props.wallImage}) center no-repeat;
+    /* background: url(${(props) => props.wallImage}) center no-repeat; */
   background-attachment: fixed;
   background-size: cover;
-  /* filter: grayscale(100%); */
+  /* filter: grayscale(50%); */
+`;
+
+export const ContentWrapper = styled.div`
+  height: 85vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+
+  >h1 {
+    color: white;
+    font-size: 5rem;
+  }
+
+  >h5 {
+    color: skyblue;
+    font-size: 3rem;
+  }
 `;
