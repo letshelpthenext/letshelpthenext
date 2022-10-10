86fe7dcfb40de540cd5851921c441aa3abf0bdc1
Kwaku Appau-Nkansah <brakwakujr@gmail.com>
2022-10-09 18:48:15 -0600

    add welcome screen with colors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Welcome from './screens/Welcome/Welcome';
 import Hero from './components/Hero/Hero';
 import HomeScreen from './screens/Home/Home';
 import GetInvolvedScreen from './screens/Get-Involved/GetInvolved';
 import OurWorkScreen from './screens/Our-Work/OurWork';
 import OurMissionScreen from './screens/Our-Mission/OurMission';
+import NotFoundScreen from './screens/NotFound/NotFound';
 import Footer from './components/Footer/Footer';
 
 function App() {
+  const location = useLocation();
   return (
     <>
       <Routes>
-        <Route path="/" element={<Hero />}>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/home" element={<Hero />}>
           <Route index element={<HomeScreen />} />
-          <Route path='get-involved' element={<GetInvolvedScreen />} />
-          <Route path='our-work' element={<OurWorkScreen />} />
-          <Route path='our-mission' element={<OurMissionScreen />} />
+          <Route path="get-involved" element={<GetInvolvedScreen />} />
+          <Route path="our-work" element={<OurWorkScreen />} />
+          <Route path="our-mission" element={<OurMissionScreen />} />
         </Route>
+        <Route path="*" element={<NotFoundScreen />} />
       </Routes>
-      <Footer />
+      { location.pathname === '/home/get-involved' && <Footer />}
+      { location.pathname === '/home/our-work' && <Footer />}
+      { location.pathname === '/home/our-mission' && <Footer />}
+      { location.pathname === '/home' && <Footer />}
     </>
   );
 }
