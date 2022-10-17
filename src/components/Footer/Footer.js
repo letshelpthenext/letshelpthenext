c627f4ec205aedd82e7a8f144feca96f9ad99acf
Kwaku Appau-Nkansah <brakwakujr@gmail.com>
2022-10-17 10:36:59 -0600

    remove phone number component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,21 +1,18 @@
 import { NavLink } from "react-router-dom";
-import { MenuWrapper, PhoneNumberWrapper, MySignWrapper, FooterWrapper } from './footer.styles'
+import { MenuWrapper, MySignWrapper, FooterWrapper } from './footer.styles'
 
 const Footer = () => {
     return (
       <FooterWrapper className="bg-success">
         <MenuWrapper>
-          <NavLink className='nav-item' to="/">Home</NavLink>
-          <NavLink className='nav-item' to="/get-involved">Get Involved</NavLink>
-          <NavLink className='nav-item' to="/our-work">Our Work</NavLink>
-          <NavLink className='nav-item' to="/our-mission">Our Mission</NavLink>
-          <a className='nav-item' target="_blank" rel="noreferrer" href="mailto:email@example.com">
+          <NavLink className='nav-item' to="/home">Home</NavLink>
+          <NavLink className='nav-item' to="/home/get-involved">Get Involved</NavLink>
+          <NavLink className='nav-item' to="/home/our-work">Our Work</NavLink>
+          <NavLink className='nav-item' to="/home/our-mission">Our Mission</NavLink>
+          {/* <a className='nav-item' target="_blank" rel="noreferrer" href="mailto:email@example.com">
             Email
-          </a>
+          </a> */}
         </MenuWrapper>
-        <PhoneNumberWrapper>
-        <a target="_blank" rel="noreferrer" href="tel:1‪(000)000-0000‬">Call: ‪(000)000-0000‬</a> | <a target="_blank" rel="noreferrer" href="sms:1‪(000)000-0000‬">Text: ‪(000)000-0000‬</a>
-        </PhoneNumberWrapper>
   
         <MySignWrapper>HELP THE NEXT (HTN) &copy; 2022 | Developed by Kwaku Appau-Nkansah</MySignWrapper>
       </FooterWrapper>
